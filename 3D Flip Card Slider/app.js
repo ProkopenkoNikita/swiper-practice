cd56cca3081b2f178a18c787edec7f46c98697a8
Nikita <nikdenpro@gmail.com>
2025-11-06 20:29:25 +0200

    final commit 3D Flip Card Slider

--- 3D Flip Card Slider/app.js
+++ 3D Flip Card Slider/app.js
@@ -1,35 +1,72 @@
 var swiper = new Swiper(".swiper", {
-    initialSlide: 3,
-    centeredSlides: true,
-    loop: true,
-    speed: 900,
-    grabCursor: true,
-    allowTouchMove: false,
-    effect: "coverflow",
-    coverflowEffect: {
-        rotate:-10,
-        stretch: -45,
-        depth: 90,
-        modifier: 1,
-        slideShadows: true,
+  initialSlide: 4,
+  centeredSlides: true,
+  loop: true,
+  speed: 900,
+  grabCursor: true,
+  allowTouchMove: false,
+  effect: "coverflow",
+  coverflowEffect: {
+    rotate: -10,
+    stretch: -45 /*gap */,
+    depth: 90 /*перспектива z */,
+    modifier: 1 /* умножение эффектов*/,
+    slideShadows: true,
+  },
+  mousewheel: {
+    thresholdDelta: 50,
+    sensitivity: 1,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+      spaceBetween: 20,
     },
-    mousewheel: {
-        thresholdDelta: 50,
-        sensitivity: 1,
+    600: {
+      slidesPerView: 3,
     },
-    pagination:{
-        el: ".swiper-pagination"
+    1200: {
+      slidesPerView: 5,
     },
-    breakpoints:{
-        0:{
-            slidesPerView:1,
-            spaceBetween: 20,
-        },
-        600:{
-            slidesPerView: 3,
-        },
-        1200:{
-            slidesPerView:5,
-        },
-    },
+  },
 });
+
+const slides = document.querySelectorAll(".swiper-slide");
+
+function flipActivesSlide() {
+  const activeSlide = document.querySelector(".swiper-slide-active");
+  const button = activeSlide.querySelector("button");
+  if (button) {
+    button.addEventListener("click", (event) => {
+      event.stopPropagation();
+      activeSlide.classList.add("flipped");
+    });
+  }
+}
+
+slides.forEach((slide) => {
+  slide.addEventListener("click", () => {
+    if (
+      slide.classList.contains("swiper-slide-active") &&
+      slide.classList.contains("flipped")
+    ) {
+        slide.classList.remove("flipped")
+    }
+  });
+});
+
+swiper.on("slideChangeTransitionStart", ()=>{
+    slides.forEach((slide) =>{
+        slide.classList.remove("flipped")
+
+    })
+    flipActivesSlide();
+})
+
+
+
+flipActivesSlide();
